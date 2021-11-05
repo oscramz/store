d6e3287e89ac8e6c1e5f16a1078d46902b4a3f32
oscramz <oscram@gmail.com>
2021-11-05 19:59:30 -0300

    Controller creation and bootstrap on yarn instalation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'categories',           to: 'categories#index',   as: 'categories'
+    get 'categories/new',       to: 'categories#show',    as: 'new_category'
+    get 'categories/:id',       to: 'categories/show',    as: 'category'
+    get 'categories/:id/edit',  to: 'categories#edit',    as: 'edit_category'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
