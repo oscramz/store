4af320260ca33271266a432ee8f0c4230acc3872
oscramz <oscram@gmail.com>
2021-11-05 21:28:50 -0300

    Categories_controller completed, admin_layout completed

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,13 @@
 class Admin::CategoriesController < ApplicationController
+
+  layout 'admin_layout'
+  before_action :set_category, only: [:show, :edit, :update, :delete]
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def show
@@ -10,4 +15,37 @@ class Admin::CategoriesController < ApplicationController
 
   def edit
   end
+
+  def create
+    @category = Category.new(params_category)
+    if @category
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @category.update(params_category)
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to admin_categories_path
+  end
+
+  private
+
+  def params_category
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'categories',           to: 'categories#index',   as: 'categories'
-    get 'categories/new',       to: 'categories#show',    as: 'new_category'
-    get 'categories/:id',       to: 'categories/show',    as: 'category'
-    get 'categories/:id/edit',  to: 'categories#edit',    as: 'edit_category'
+    get     'categories',           to: 'categories#index',   as: 'categories'
+    get     'categories/new',       to: 'categories#show',    as: 'new_category'
+    get     'categories/:id',       to: 'categories#show',    as: 'category'
+    get     'categories/:id/edit',  to: 'categories#edit',    as: 'edit_category'
+
+    post    'categories',           to: 'categories#create'
+    put     'categories/:id',        to: 'categories#update'
+    patch   'categories/:id',       to: 'categories#update'
+    delete  'categories/:id',       to: 'categories#destroy'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
